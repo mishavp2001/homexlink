09160896d5dbf5f37b303c1bc9cb79ab5c0c5428
michael <michael@michaels-MacBook-Pro.local>
2026-03-12 09:41:31 -0700

    feat: add social setup

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -4,16 +4,22 @@ export const auth = defineAuth({
   loginWith: {
     email: true,
     externalProviders: {
-      callbackUrls: ['http://localhost:3000/'],
-      logoutUrls: ['http://localhost:3000/'],
+       callbackUrls: [
+        'https://homexlink.com',
+        'https://localhost.com',
+      ],
+      logoutUrls: [
+        'https://homexlink.com',
+        'https://localhost.com',
+      ],
       google: { clientId: secret('GOOGLE_CLIENT_ID'), clientSecret: secret('GOOGLE_CLIENT_SECRET') },
       facebook: { clientId: secret('FACEBOOK_CLIENT_ID'), clientSecret: secret('FACEBOOK_CLIENT_SECRET') },
-      signInWithApple: {
+/*     signInWithApple: {
         clientId: secret('APPLE_CLIENT_ID'),
         keyId: secret('APPLE_KEY_ID'),
         privateKey: secret('APPLE_PRIVATE_KEY'),
         teamId: secret('APPLE_TEAM_ID'),
-      },
+      }, */
     },
   },
   groups: ['ADMINS'],
